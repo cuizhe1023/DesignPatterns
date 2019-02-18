5aca92f214796d400df60ad5942cf0fc0caba4a9
cuizhe1023 <fight.1214@qq.com>
2019-02-18 22:31:18 +0800

    添加了单例模式:懒汉模式(线程不安全)下多线程的测试情况。

--- Singleton/src/lazyType_unsafe/LazyTypeUnsafeTest.java
+++ Singleton/src/lazyType_unsafe/LazyTypeUnsafeTest.java
@@ -6,12 +6,47 @@ package lazyType_unsafe;
  */
 public class LazyTypeUnsafeTest {
 
+    /*
     public static void main(String[] args) {
-
+        //在单线程的情况下运行
         Singleton singleton = Singleton.getInstance();
         Singleton singleton1 = Singleton.getInstance();
-
-        System.out.println("两次创建的对象是否相等:"+(singleton==singleton1));
+        System.out.println("两次创建的对象是否相等:"+(singleton==singleton1));//运行结果一直都是true,说明单线程下是没问题的
     }
+    */
 
+    public static void main(String[] args) {
+        //在多线程的情况下运行
+        ThreadTest test = new ThreadTest();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+        new Thread(test).start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(test.singles);
+
+        /*
+        还没有创建了实例化对象
+        创建了实例化对象
+        还没有创建了实例化对象
+        创建了实例化对象
+        还没有创建了实例化对象
+        创建了实例化对象
+        [lazyType_unsafe.Singleton@68553a8d, lazyType_unsafe.Singleton@2633e42b, lazyType_unsafe.Singleton@7941b718]
+         */
+        //说明有线程并发访问安全问题.
+
+    }
 }
